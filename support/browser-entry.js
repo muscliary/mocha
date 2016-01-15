muscliary/mocha

commit a81e5558f086808b682b6d196b80893d2c8926f8

Build using Browserify
Signed-off-by: Joshua Appelman <jappelman@xebia.com>

Closes #1193

support/browser-entry.js
@@ -1,5 +1,12 @@
-// The global object is "self" in Web Workers.
-var global = (function() { return this; })();
+var Mocha = require('../');
+
+/**
+ * Create a Mocha instance.
+ *
+ * @return {undefined}
+ */
+
+var mocha = new Mocha({ reporter: 'html' });
 
 /**
  * Save timer references to avoid Sinon interfering (see GH-237).
@@ -10,19 +17,6 @@ var setTimeout = global.setTimeout;
 var setInterval = global.setInterval;
 var clearTimeout = global.clearTimeout;
 var clearInterval = global.clearInterval;
-
-/**
- * Node shims.
- *
- * These are meant only to allow
- * mocha.js to run untouched, not
- * to allow running node code in
- * the browser.
- */
-
-var process = {};
-process.exit = function(status){};
-process.stdout = {};
 
 var uncaughtExceptionHandlers = [];
 
@@ -58,13 +52,6 @@ process.on = function(e, fn){
     uncaughtExceptionHandlers.push(fn);
   }
 };
-
-/**
- * Expose mocha.
- */
-
-var Mocha = global.Mocha = require('mocha'),
-    mocha = global.mocha = new Mocha({ reporter: 'html' });
 
 // The BDD UI is registered by default, but no UI will be functional in the
 // browser without an explicit call to the overridden `mocha.ui` (see below).
@@ -154,7 +141,21 @@ mocha.run = function(fn){
 };
 
 /**
+ * Shim process.stdout.
+ */
+
+process.stdout = require('browser-stdout')();
+
+/**
  * Expose the process shim.
+ * https://github.com/mochajs/mocha/pull/916
  */
 
 Mocha.process = process;
+
+/**
+ * Expose mocha.
+ */
+
+window.Mocha = Mocha;
+window.mocha = mocha;

Makefile
@@ -1,4 +1,6 @@
+BROWSERIFY := node_modules/.bin/browserify
 ESLINT := node_modules/.bin/eslint
+
 REPORTER ?= spec
 TM_BUNDLE = JavaScript\ mocha.tmbundle
 SRC = $(shell find lib -name "*.js" -type f | sort)
@@ -6,20 +8,15 @@ SUPPORT = $(wildcard support/*.js)
 
 all: mocha.js
 
-lib/browser/diff.js: node_modules/diff/diff.js
-	cp node_modules/diff/diff.js lib/browser/diff.js
-
-lib/browser/escape-string-regexp.js: node_modules/escape-string-regexp/index.js
-	cp node_modules/escape-string-regexp/index.js lib/browser/escape-string-regexp.js
-
-mocha.js: $(SRC) $(SUPPORT) lib/browser/diff.js lib/browser/escape-string-regexp.js
-	@node support/compile $(SRC)
-	@cat \
-	  support/head.js \
-	  _mocha.js \
-	  support/tail.js \
-	  support/foot.js \
-	  > mocha.js
+# TODO: Remove filesize echos, just for comparison during browserify transition
+mocha.js: $(SRC) $(SUPPORT)
+	@$(BROWSERIFY) ./support/browser-entry \
+		--ignore 'fs' \
+		--ignore 'glob' \
+		--ignore 'jade' \
+		--ignore 'path' \
+		--ignore 'supports-color' \
+		--exclude './lib-cov/mocha' > $@
 
 clean:
 	rm -f mocha.js

lib/browser/escape-string-regexp.js
@@ -1,21 +1,0 @@
-'use strict';
-
-/**
- * Expose `escape`.
- */
-
-module.exports = escape;
-
-var matchOperatorsRe = /[|\\{}()[\]^$+*?.]/g;
-
-/**
- * @param {string} str
- * @return {string}
- */
-function escape(str) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string');
-  }
-
-  return str.replace(matchOperatorsRe, '\\$&');
-}

lib/mocha.js
@@ -10,6 +10,7 @@
 
 var escapeRe = require('escape-string-regexp');
 var path = require('path');
+var reporters = require('./reporters');
 var utils = require('./utils');
 
 /**
@@ -22,7 +23,7 @@ exports = module.exports = Mocha;
  * To require local UIs and reporters when running in node.
  */
 
-if (typeof process !== 'undefined' && typeof process.cwd === 'function') {
+if (!process.browser) {
   var cwd = process.cwd();
   module.paths.push(cwd, path.join(cwd, 'node_modules'));
 }
@@ -33,7 +34,7 @@ if (typeof process !== 'undefined' && typeof process.cwd === 'function') {
 
 exports.utils = utils;
 exports.interfaces = require('./interfaces');
-exports.reporters = require('./reporters');
+exports.reporters = reporters;
 exports.Runnable = require('./runnable');
 exports.Context = require('./context');
 exports.Runner = require('./runner');
@@ -150,11 +151,11 @@ Mocha.prototype.reporter = function(reporter, reporterOptions) {
   } else {
     reporter = reporter || 'spec';
     var _reporter;
-    try {
-      _reporter = require('./reporters/' + reporter);
-    } catch (err) {
-      // Ignore
+    // Try to load a built-in reporter.
+    if (reporters[reporter]) {
+      _reporter = reporters[reporter];
     }
+    // Try to load reporters from process.cwd() and node_modules
     if (!_reporter) {
       try {
         _reporter = require(reporter);

lib/reporters/base.js
@@ -6,7 +6,7 @@ var tty = require('tty');
 var diff = require('diff');
 var ms = require('../ms');
 var utils = require('../utils');
-var supportsColor = process.env ? require('supports-color') : null;
+var supportsColor = process.browser ? null : require('supports-color');
 
 /**
  * Expose `Base`.
@@ -37,9 +37,7 @@ var isatty = tty.isatty(1) && tty.isatty(2);
  * Enable coloring by default, except in the browser interface.
  */
 
-exports.useColors = process.env
-  ? (supportsColor || (process.env.MOCHA_COLORS !== undefined))
-  : false;
+exports.useColors = !process.browser && (supportsColor || (process.env.MOCHA_COLORS !== undefined));
 
 /**
  * Inline diffs instead of +/-

lib/reporters/html.js
@@ -7,7 +7,7 @@
 var Base = require('./base');
 var utils = require('../utils');
 var Progress = require('../browser/progress');
-var escapeRe = require('../browser/escape-string-regexp');
+var escapeRe = require('escape-string-regexp');
 var escape = utils.escape;
 
 /**

lib/reporters/index.js
@@ -1,17 +1,19 @@
-exports.Base = require('./base');
-exports.Dot = require('./dot');
-exports.Doc = require('./doc');
-exports.TAP = require('./tap');
-exports.JSON = require('./json');
-exports.HTML = require('./html');
-exports.List = require('./list');
-exports.Min = require('./min');
-exports.Spec = require('./spec');
-exports.Nyan = require('./nyan');
-exports.XUnit = require('./xunit');
-exports.Markdown = require('./markdown');
-exports.Progress = require('./progress');
-exports.Landing = require('./landing');
-exports.JSONCov = require('./json-cov');
-exports.HTMLCov = require('./html-cov');
-exports.JSONStream = require('./json-stream');
+// Alias exports to a their normalized format Mocha#reporter to prevent a need
+// for dynamic (try/catch) requires, which Browserify doesn't handle.
+exports.Base = exports.base = require('./base');
+exports.Dot = exports.dot = require('./dot');
+exports.Doc = exports.doc = require('./doc');
+exports.TAP = exports.tap = require('./tap');
+exports.JSON = exports.json = require('./json');
+exports.HTML = exports.html = require('./html');
+exports.List = exports.list = require('./list');
+exports.Min = exports.min = require('./min');
+exports.Spec = exports.spec = require('./spec');
+exports.Nyan = exports.nyan = require('./nyan');
+exports.XUnit = exports.xunit = require('./xunit');
+exports.Markdown = exports.markdown = require('./markdown');
+exports.Progress = exports.progress = require('./progress');
+exports.Landing = exports.landing = require('./landing');
+exports.JSONCov = exports['json-cov'] = require('./json-cov');
+exports.HTMLCov = exports['html-cov'] = require('./html-cov');
+exports.JSONStream = exports['json-stream'] = require('./json-stream');

lib/runner.js
@@ -736,7 +736,7 @@ function filterLeaks(ok, globals) {
  * @return {Array}
  */
 function extraGlobals() {
-  if (typeof process === 'object' && typeof process.version === 'string') {
+  if (!process.browser) {
     var nodeVersion = process.version.split('.').reduce(function(a, v) {
       return a << 8 | v;
     });

commit 821d7a583f26103660893fb20369c537600818cd

Have global exports be compatible with Web Workers
Assigning to `window` only works in a normal browser environment;
however, Web Workers don't have access to `window`. Instead, the global
object inside Web Workers is `self`.

Browserify ensures that `global` is present to all scripts by creating a
shim reference to it using this logic:

    typeof global !== "undefined" ? global :
    typeof self !== "undefined" ? self :
    typeof window !== "undefined" ? window : {}

Thus, if neither `global` nor `self` were originally present,
`global === window` will be true.

Web Workers compatibility was broken in a81e5558.

support/browser-entry.js
@@ -157,5 +157,5 @@ Mocha.process = process;
  * Expose mocha.
  */
 
-window.Mocha = Mocha;
-window.mocha = mocha;
+global.Mocha = Mocha;
+global.mocha = mocha;